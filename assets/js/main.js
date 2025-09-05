98c35c3a903fcf6aac915ef2ab03301dc37741a0
Mostafa <m.mohammadimehr1390@gmail.com>
2025-09-05 15:23:08 +0330

    fetch2 #1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,1 @@
-const mybtn = document.getElementById("gettext");
-let FEtch = () => {
-  fetch("/sample.txt")
-    .then((res) => res.text())
-    .then((data) => console.log(data));
-};
-mybtn.addEventListener("click", FEtch);
+
