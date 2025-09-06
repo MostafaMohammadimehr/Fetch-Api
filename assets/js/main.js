180ec2d312c047397d05d6931b221e6d20513834
Mostafa <m.mohammadimehr1390@gmail.com>
2025-09-06 15:32:37 +0330

    fetch api  post #2

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+document.getElementById("addPost").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let TitleValue = document.getElementById("title").value;
+  let BodyValue = document.getElementById("body").value;
+
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    headers: {
+      "Accept": "application/json, text/plain, */*",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ title: TitleValue, body: BodyValue }),
+  })
+    .then((res) => res.json())
+    .then((json) => console.log(json));
+});
