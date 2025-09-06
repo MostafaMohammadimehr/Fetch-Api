6e48164290ac4fa310c342bf1f757fd40df76669
Mostafa <m.mohammadimehr1390@gmail.com>
2025-09-06 12:04:59 +0330

    fetch api  li #2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,15 @@
 document.getElementById("mybtn").addEventListener("click", () => {
   fetch("/assets/json/users.json")
     .then((res) => res.json())
-    .then((json) => console.log(json));
+    .then((json) => {
+      let output = "";
+      json.forEach(function (user) {
+        output += `<ul>
+        <li> ${user.id} </li>
+        <li> ${user.name} </li>
+        <li> ${user.email} </li>
+        </ul>`;
+      });
+      document.getElementById("output").innerHTML = output;
+    });
 });
